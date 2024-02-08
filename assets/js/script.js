51dc9045323003e7f6fdd694844da3acd4d76e16
Sigrid Müller <sigrid.muller.vidal@gmail.com>
2024-02-08 17:57:21 -0300

    modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
-//POPOVER CODE
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+document.addEventListener("DOMContentLoaded", function() {
 
+    // Modal
+    const myModal = document.getElementById('exampleModal');
+
+    myModal.addEventListener('shown.bs.modal', () => {
+      const firstInput = myModal.querySelector('input, button');
+      if (firstInput) {
+        firstInput.focus();
+      }
+    });
+  });
